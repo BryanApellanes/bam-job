f13b1163099cbadf91f5070614ed8350cb9495e0
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-01-16 08:41:40 -0800

    update to main

---
+++ main.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const artifact = require('@actions/artifact');
+const bam = require("@bamapps/bam.js");
+
+try {
+    console.log('this is not implemented')
+} catch (error) {
+
+}
